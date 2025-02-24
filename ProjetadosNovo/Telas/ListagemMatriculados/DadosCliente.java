e59110ae1b91a3aff3cdb31cbbc0ac3beae9898f
Evilayne <evilayne.ribeiro@academico.ifpb.edu.br>
2025-02-24 16:43:20 -0300

    ajustes gerais

--- ProjetadosNovo/Telas/ListagemMatriculados/DadosCliente.java
+++ ProjetadosNovo/Telas/ListagemMatriculados/DadosCliente.java
@@ -25,51 +25,51 @@ public class DadosCliente extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
-        jLabel12 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        lblnome = new javax.swing.JLabel();
+        lblnomecliente = new javax.swing.JLabel();
+        lblemail = new javax.swing.JLabel();
+        lblemailcliente = new javax.swing.JLabel();
+        lbldata = new javax.swing.JLabel();
+        lbldatamatricula = new javax.swing.JLabel();
+        lbltelefone = new javax.swing.JLabel();
+        lbltelefonenum = new javax.swing.JLabel();
+        lblplano = new javax.swing.JLabel();
+        lblplanotipo = new javax.swing.JLabel();
+        lblsituacao = new javax.swing.JLabel();
+        lblsituacaotipo = new javax.swing.JLabel();
+        btntreino = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
-        jLabel1.setText("Nome:");
+        lblnome.setText("Nome:");
 
-        jLabel2.setText("Denis Willian");
+        lblnomecliente.setText("Denis Willian");
 
-        jLabel3.setText("Email:");
+        lblemail.setText("Email:");
 
-        jLabel4.setText("deniswillian@gmail.com");
+        lblemailcliente.setText("deniswillian@gmail.com");
 
-        jLabel5.setText("Data da Matricula:");
+        lbldata.setText("Data da Matricula:");
 
-        jLabel6.setText("01/01/2025");
+        lbldatamatricula.setText("01/01/2025");
 
-        jLabel7.setText("Categoria:");
+        lbltelefone.setText("Telefone:");
 
-        jLabel8.setText("Itermediário");
+        lbltelefonenum.setText("00 00000 0000");
 
-        jLabel9.setText("Plano:");
+        lblplano.setText("Plano:");
 
-        jLabel10.setText("Mensal");
+        lblplanotipo.setText("Mensal");
 
-        jLabel11.setText("Situação da Matricula:");
+        lblsituacao.setText("Situação da Matricula:");
 
-        jLabel12.setText("Ativa");
+        lblsituacaotipo.setText("Ativa");
 
-        jButton1.setText("Treinos");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btntreino.setText("Treinos");
+        btntreino.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btntreinoActionPerformed(evt);
             }
         });
 
@@ -77,73 +77,73 @@ public class DadosCliente extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btntreino)
+                .addGap(20, 20, 20))
             .addGroup(layout.createSequentialGroup()
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblnome, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(lblnomecliente, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel9)
+                        .addComponent(lblplano)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel10)))
+                        .addComponent(lblplanotipo)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
+                        .addComponent(lblemail)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(lblemailcliente, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel7)
+                        .addComponent(lbltelefone)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel8)))
+                        .addComponent(lbltelefonenum)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5)
+                        .addComponent(lbldata)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel6))
+                        .addComponent(lbldatamatricula))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel11)
+                        .addComponent(lblsituacao)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel12, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap(31, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(20, 20, 20))
+                        .addComponent(lblsituacaotipo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6))
+                    .addComponent(lblnome)
+                    .addComponent(lblnomecliente)
+                    .addComponent(lblemail)
+                    .addComponent(lblemailcliente)
+                    .addComponent(lbldata)
+                    .addComponent(lbldatamatricula))
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel8)
-                    .addComponent(jLabel9)
-                    .addComponent(jLabel10)
-                    .addComponent(jLabel11)
-                    .addComponent(jLabel12))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                    .addComponent(lbltelefone)
+                    .addComponent(lbltelefonenum)
+                    .addComponent(lblplano)
+                    .addComponent(lblplanotipo)
+                    .addComponent(lblsituacao)
+                    .addComponent(lblsituacaotipo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
+                .addComponent(btntreino)
                 .addGap(17, 17, 17))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btntreinoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btntreinoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btntreinoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -181,18 +181,18 @@ public class DadosCliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
+    private javax.swing.JButton btntreino;
+    private javax.swing.JLabel lbldata;
+    private javax.swing.JLabel lbldatamatricula;
+    private javax.swing.JLabel lblemail;
+    private javax.swing.JLabel lblemailcliente;
+    private javax.swing.JLabel lblnome;
+    private javax.swing.JLabel lblnomecliente;
+    private javax.swing.JLabel lblplano;
+    private javax.swing.JLabel lblplanotipo;
+    private javax.swing.JLabel lblsituacao;
+    private javax.swing.JLabel lblsituacaotipo;
+    private javax.swing.JLabel lbltelefone;
+    private javax.swing.JLabel lbltelefonenum;
     // End of variables declaration//GEN-END:variables
 }
