5ea224a4820345b31f5bcf073a985b1cacd4effe
paulogx77 <paulo.guilhermeez@gmail.com>
2025-02-25 16:13:32 -0300

    update

--- ProjetadosNovo/Telas/TelaLogin/Login.java
+++ ProjetadosNovo/Telas/TelaLogin/Login.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Login {
+import javax.swing.*;
+
+public class Login {
+    public static void main(String[] args) {
+        TelaLoginNETBEANS login = new TelaLoginNETBEANS();
+        login.setVisible(true);
+        login.pack();
+        login.setLocationRelativeTo(null);
+        login.setResizable(false);
+    }
 }
